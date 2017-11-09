25d36a0c214e79a1278142983497dd4b4cb97698
Jepno <jepno@dtu.dk>
2017-11-09 12:07:27 +0100

    Added evaluation scores and created an xarray DataFrame (pandas multidimensional brother) for holding results. They are easier to print and understand now.

--- run_files/loo_cv.py
+++ run_files/loo_cv.py
@@ -1,24 +1,32 @@
 import numpy as np
 import tensorflow as tf
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import LeaveOneOut
+import xarray as xr
 
 from models.model_base import DetektorModel
 from models.baselines import MLP, LogisticRegression
+from evaluations import Accuracy, F1, TruePositives, TrueNegatives, FalsePositives, FalseNegatives, Samples
 from util.tensor_provider import TensorProvider
 
 
-def leave_one_program_out_cv(tensor_provider, model_list, eval_functions=None, limit=None):
+def leave_one_program_out_cv(tensor_provider, model_list, eval_functions=None, limit=None, return_predictions=False):
     """
-    :param TensorProvider tensor_provider:
-    :param list[ClassVar] model_list:
-    :param list[Callable] eval_functions:
+    :param TensorProvider tensor_provider: Class providing all data to models.
+    :param list[ClassVar] model_list: List of model-classes for testing.
+    :param list[Evaluation] eval_functions: List of evaluation functions used to test models.
+    :param bool return_predictions: If True, the method stores all model test-predictions and returns them as well.
+                                    Can be used to determine whether errors are the same across models.
+    :param int | None limit: Only perform analysis on some programs (for testing)
+                             If None - run on all programs.
     :return:
     """
+    n_models = len(model_list)
 
     # Default evaluation score
     if eval_functions is None:
-        eval_functions = [accuracy_score]
+        eval_functions = [Accuracy(), F1(), TruePositives(), TrueNegatives(), FalsePositives(), FalseNegatives(),
+                          Samples()]
+    n_evaluations = len(eval_functions)
 
     # Elements keys
     keys = tensor_provider.keys
@@ -27,9 +35,19 @@ def leave_one_program_out_cv(tensor_provider, model_list, eval_functions=None, l
     program_ids = np.array(list(zip(*keys))[0])
     unique_programs = np.array(sorted(set(program_ids)))
     n_programs = len(unique_programs)
+    program_names = ["P{:02d}".format(val + 1) for val in range(n_programs)]
+
+    # Dictionary for holding actual predictions (they vary in length which discourages an array)
+    test_predictions = dict()
 
     # Initialize array for holding results
-    classification_results = np.full((n_programs, len(model_list), len(eval_functions)), np.nan)
+    classification_results = np.full((n_programs, n_models, n_evaluations), np.nan)
+    classification_results = xr.DataArray(classification_results,
+                                          name="Loo Results",
+                                          dims=["Program", "Model", "Evaluation"],
+                                          coords=dict(Program=program_names,
+                                                      Model=[model_class.name() for model_class in model_list],
+                                                      Evaluation=[val.name() for val in eval_functions]))
 
     # TODO: Is this loop-ordering counter-intuitive? Would it be better to train and measure each model once at a time?
 
@@ -38,6 +56,8 @@ def leave_one_program_out_cv(tensor_provider, model_list, eval_functions=None, l
     limit = len(unique_programs) if limit is None else limit
     print("\n\nRunning Leave-One-Out Tests.\n" + "-" * 75)
     for program_nr, (train, test) in enumerate(list(loo.split(unique_programs))[:limit]):
+        program_name = program_names[program_nr]
+
         # Get split indices
         train_idx = np.where(program_ids != unique_programs[test])[0]
         test_idx = np.where(program_ids == unique_programs[test])[0]
@@ -64,25 +84,37 @@ def leave_one_program_out_cv(tensor_provider, model_list, eval_functions=None, l
 
         # Go through models
         for model_nr, model_class in enumerate(model_list):
+            model_name = model_class.name()
+            print(" " * 2 + "Model: {}".format(model_name))
+
             # Initialize TF.session... and clear previous?
             tf_sess = tf.Session()
 
             # Initialize model
             model = model_class()  # type: DetektorModel
-            print(" " * 2 + "Model: {}".format(model.name))
 
             # Fit model
             model.fit(training_data, tf_sess, indentation=4)
 
             # Predict on test-data for performance
-            y_pred = model.predict(test_data, tf_sess)
+            y_pred = np.squeeze(model.predict(test_data, tf_sess))
+            y_true = test_data['labels']
+
+            # Store predictions
+            if return_predictions:
+                test_predictions.setdefault(model_name, dict())[program_name] = y_pred
 
             # Evaluate with eval_functions
             for evaluation_nr, evalf in enumerate(eval_functions):
-                classification_results[program_nr, model_nr, evaluation_nr] = evalf(test_data['labels'], y_pred)
+                assert y_pred.shape == y_true.shape, "y_pred ({}) and y_true ({}) " \
+                                                     "does not have same shape".format(y_pred.shape, y_true.shape)
+                evaluation_results = evalf(y_true, y_pred)
+                classification_results[program_nr, model_nr, evaluation_nr] = evaluation_results
 
         print(" " * 2 + "Done with program {}".format(program_nr + 1))
 
+    if return_predictions:
+        return classification_results, test_predictions
     return classification_results
 
 
@@ -91,7 +123,10 @@ if __name__ == "__main__":
 
     results = leave_one_program_out_cv(tensor_provider=the_tensor_provider,
                                        model_list=[LogisticRegression, MLP],
-                                       limit=None)
+                                       limit=None)  # type: xr.DataArray
 
-    print("\nResults\n" + "-" * 75)
-    print(results)
+    mean_results = results.mean("Program")
+    mean_results.name = "Mean Loo Results"
+
+    print("\nMean LOO Results\n" + "-" * 75)
+    print(mean_results._to_dataset_split("Model").to_dataframe())
