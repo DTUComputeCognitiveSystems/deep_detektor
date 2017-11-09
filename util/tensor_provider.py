9bf6a66479cd58284af3a7eeb50f88c56ed38642
Jepno <jepno@dtu.dk>
2017-11-09 09:49:29 +0100

    Encapsulated tf.Graph of tensor_provider. Fixed sample-labelling. tensor_provider can now use specific vocabularies for BoW (instead of both test and training set).

--- util/tensor_provider.py
+++ util/tensor_provider.py
@@ -13,12 +13,12 @@ from project_paths import embeddings_file, pos_tags_file, speller_results_file, 
 
 
 class TensorProvider:
-    def __init__(self, tf_graph, tf_session, verbose=False, end_of_word_char="$", fill=np.nan):
-        # Make graph and session
+    def __init__(self, verbose=False, end_of_word_char="$", fill=np.nan):
         self.fill = fill
-        self._tf_graph = tf_graph  # tf.Graph() if tf_graph is None else tf_graph
-        self._sess = tf_session  # tf.Session() if tf_session is None else tf_session
-        self._sess.run(tf.global_variables_initializer())
+
+        # Make graph and session
+        self._tf_graph = tf.Graph()
+        self._sess = tf.Session(graph=self._tf_graph)
 
         ###################
         # Character embedding (auto-encoder)
@@ -32,14 +32,15 @@ class TensorProvider:
         with speller_translator_file.open("r") as file:
             self.string_translator = {int(val[0]): val[1] for val in json.load(file).items()}
 
-        self.speller_dimensions = speller_results['cells']
-        self.recurrent_speller = RecurrentSpeller(n_inputs=len(self.char_embedding),
-                                                  n_outputs=len(self.char_embedding),
-                                                  n_encoding_cells=self.speller_dimensions,
-                                                  n_decoding_cells=self.speller_dimensions,
-                                                  character_embedding=self.char_embedding,
-                                                  end_of_word_char=end_of_word_char)
-        self.recurrent_speller.load_encoder(sess=self._sess, file_path=speller_encoder_checkpoint_file)
+        with self._tf_graph.as_default():
+            self.char_embedding_size = speller_results['cells']
+            self.recurrent_speller = RecurrentSpeller(n_inputs=len(self.char_embedding),
+                                                      n_outputs=len(self.char_embedding),
+                                                      n_encoding_cells=self.char_embedding_size,
+                                                      n_decoding_cells=self.char_embedding_size,
+                                                      character_embedding=self.char_embedding,
+                                                      end_of_word_char=end_of_word_char)
+            self.recurrent_speller.load_encoder(sess=self._sess, file_path=speller_encoder_checkpoint_file)
 
         ###################
         # Labels of original data
@@ -58,7 +59,7 @@ class TensorProvider:
             for row in csv_reader:
                 key = (int(row[2]), int(row[3]))
                 self.keys.append(key)
-                self.labels[key] = row[6]
+                self.labels[key] = bool(row[6])
 
         ###################
         # Word embeddings
@@ -70,7 +71,9 @@ class TensorProvider:
             csv_reader = csv.reader(file, delimiter=",")
             for row in csv_reader:
                 self.word_embeddings[row[0]] = np.array(eval(row[1]))
-        self.word_embedding_length = len(self.word_embeddings[list(self.word_embeddings.keys())[0]])
+
+        # Word embedding length (+1 due to flag for unknown vectors)
+        self.word_embedding_size = len(self.word_embeddings[list(self.word_embeddings.keys())[0]]) + 3
 
         ###################
         # Tokenized texts and POS-tags
@@ -94,6 +97,7 @@ class TensorProvider:
                 self.tokens[key] = tokens
         self.pos_vocabulary = {val: idx for idx, val in enumerate(sorted(list(self.pos_vocabulary)))}
         self.vocabulary = {val: idx for idx, val in enumerate(sorted(list(self.vocabulary)))}
+        self.pos_embedding_size = len(self.pos_vocabulary)
         self._pos_embedding = np.eye(len(self.pos_vocabulary), len(self.pos_vocabulary))
 
         ###################
@@ -105,10 +109,10 @@ class TensorProvider:
 
         # The set of known words
         if self.bow_transformer is None:
-            self.bow_vocabulary = self.vocabulary
+            self.complete_bow_vocabulary = self.vocabulary
         else:
             bow_vocabulary = {self.bow_transformer(word) for word in self.vocabulary.keys()}
-            self.bow_vocabulary = {word: idx for idx, word in enumerate(sorted(list(bow_vocabulary)))}
+            self.complete_bow_vocabulary = {word: idx for idx, word in enumerate(sorted(list(bow_vocabulary)))}
 
     def _get_known_word(self, word: str):
         operations = [
@@ -133,10 +137,11 @@ class TensorProvider:
             n_words = max([len(val) for val in tokens])
 
         # Initialize array
-        out_array = np.full((len(tokens), n_words, self.word_embedding_length + 1), self.fill)
+        out_array = np.full((len(tokens), n_words, self.word_embedding_size), self.fill)
 
         # Default vector (for unknown words)
-        default_vector = np.array([0] * self.word_embedding_length + [1])
+        unknown_vector = np.array([0] * self.word_embedding_size)
+        unknown_vector[-1] = 1
 
         # Insert word-vectors
         for text_nr, text in enumerate(tokens):
@@ -146,7 +151,7 @@ class TensorProvider:
                     c_embedding = self.word_embeddings[c_word]
                     out_array[text_nr, word_nr, :-1] = c_embedding
                 else:
-                    out_array[text_nr, word_nr, :] = default_vector
+                    out_array[text_nr, word_nr, :] = unknown_vector
 
         # Return
         return out_array
@@ -166,7 +171,7 @@ class TensorProvider:
 
     def _get_character_embedding(self, data_keys, n_words):
         # Initialize array
-        out_array = np.full((len(data_keys), n_words, self.speller_dimensions), self.fill)
+        out_array = np.full((len(data_keys), n_words, self.char_embedding_size), self.fill)
 
         # Go through elements
         for key_nr, key in enumerate(data_keys):
@@ -182,53 +187,102 @@ class TensorProvider:
 
         return out_array
 
-    def _get_bow_tensors(self, tokens):
+    def _get_bow_tensors(self, tokens, bow_vocabulary=None):
+        if bow_vocabulary is None:
+            bow_vocabulary = self.complete_bow_vocabulary
 
         # Transform data if wanted
         if self.bow_transformer is not None:
             tokens = [[self.bow_transformer(word) for word in text] for text in tokens]
 
         # Initialize array
-        out_array = np.full((len(tokens), len(self.bow_vocabulary)), 0)
+        out_array = np.full((len(tokens), len(bow_vocabulary)), 0)
 
         # Go through texts and words
         for text_nr, text in enumerate(tokens):
             text_bow = Counter()
-            text_bow.update([self.bow_vocabulary[word] for word in text if word in self.bow_vocabulary])
+            text_bow.update([bow_vocabulary[word] for word in text if word in bow_vocabulary])
             word_idxs, word_counts = zip(*text_bow.items())
 
             out_array[text_nr, word_idxs] = word_counts
 
         return out_array
 
-    def load_data_tensors(self, data_keys_or_idxs):
-        data_tensors = dict()
+    def extract_programs_vocabulary(self, train_keys_or_idxs):
+        train_keys_or_idxs = self._convert_to_keys(train_keys_or_idxs)
 
-        if isinstance(data_keys_or_idxs[0], int):
+        # Get tokens of dataset
+        tokens = [self.tokens[val] for val in train_keys_or_idxs]
+
+        # Make vocabulary of dataset
+        vocabulary = set()
+        for text in tokens:
+            vocabulary.update(text)
+
+        # Make vocabulary
+        bow_vocabulary = {self.bow_transformer(word) for word in vocabulary}
+        bow_vocabulary = {word: idx for idx, word in enumerate(sorted(list(bow_vocabulary)))}
+
+        return bow_vocabulary
+
+    def _convert_to_keys(self, data_keys_or_idxs):
+        if isinstance(data_keys_or_idxs[0], (int, np.int32, np.int64)):
             data_keys_or_idxs = [self.keys[val] for val in data_keys_or_idxs]
 
+        return data_keys_or_idxs
+
+    def _get_labels(self, data_keys):
+        return np.array([self.labels[key] for key in data_keys])
+
+    def sequence_dimensions(self, word_embedding=True, pos_tags=True, char_embedding=True):
+        d = 0
+
+        if word_embedding:
+            d += self.word_embedding_size
+
+        if pos_tags:
+            d += self.pos_embedding_size
+
+        if char_embedding:
+            d += self.char_embedding_size
+
+    def load_data_tensors(self, data_keys_or_idx, bow_vocabulary=None,
+                          word_embedding=True, pos_tags=True, char_embedding=True, bow=True,
+                          labels=True):
+        data_tensors = dict()
+
+        data_keys = self._convert_to_keys(data_keys_or_idx)
+
         # Get tokens of data-query
-        tokens = [self.tokens[val] for val in data_keys_or_idxs]
+        tokens = [self.tokens[val] for val in data_keys]
 
         # Determine number of words in each sample
         n_words = max([len(val) for val in tokens])
 
         # Word embeddings
-        data_tensors["word_embedding"] = self._get_word_embeddings(tokens=tokens, n_words=n_words)
+        if word_embedding:
+            data_tensors["word_embedding"] = self._get_word_embeddings(tokens=tokens, n_words=n_words)
 
         # Pos tags
-        data_tensors["pos_tags"] = self._get_pos_tags(data_keys=data_keys_or_idxs, n_words=n_words)
+        if pos_tags:
+            data_tensors["pos_tags"] = self._get_pos_tags(data_keys=data_keys, n_words=n_words)
 
         # Character embedding
-        data_tensors["char_embedding"] = self._get_character_embedding(data_keys=data_keys_or_idxs, n_words=n_words)
+        if char_embedding:
+            data_tensors["char_embedding"] = self._get_character_embedding(data_keys=data_keys, n_words=n_words)
 
         # BoW representations
-        data_tensors["bow"] = self._get_bow_tensors(tokens=tokens)
+        if bow:
+            data_tensors["bow"] = self._get_bow_tensors(tokens=tokens, bow_vocabulary=bow_vocabulary)
+
+        # Data labels
+        if labels:
+            data_tensors["labels"] = self._get_labels(data_keys=data_keys)
 
         return data_tensors
 
 
 if __name__ == "__main__":
-    tensor_provider = TensorProvider(tf_graph=tf.Graph(), tf_session=tf.Session(), verbose=True)
+    tensor_provider = TensorProvider(verbose=True)
     print("\nTesting tensor provider.")
     test = tensor_provider.load_data_tensors([0, 3, 4, 6])
