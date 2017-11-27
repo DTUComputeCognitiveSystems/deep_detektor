e7ddd658c16a1400847828cc40a72cbe898883cc
sfvnDTU <sfvn0410@gmail.com>
2017-11-27 10:59:37 +0100

    Implemented stratified batch sampling in basic recurrent - will be updated to be more general

--- models/recurrent/basic_recurrent.py
+++ models/recurrent/basic_recurrent.py
@@ -8,7 +8,8 @@ from util.training_utilities import linear_geometric_curve
 
 class BasicRecurrent(DetektorModel):
     def __init__(self, tensor_provider, units=None,
-                 word_embedding=True, pos_tags=True, char_embedding=True):
+                 word_embedding=True, pos_tags=True, char_embedding=True,
+                 optimizer=tf.train.GradientDescentOptimizer):
         """
 
         :param TensorProvider tensor_provider: Provides data for model.
@@ -37,6 +38,8 @@ class BasicRecurrent(DetektorModel):
             self.inputs = tf.placeholder(tf.float32, shape=[None, None, self.num_features], name='input')
             self.input_lengths = tf.placeholder(tf.int32, shape=[None], name='input_length')
             self.truth = tf.placeholder(tf.float32, [None, ])
+            # TODO: Introduce class weights in cost
+            # self.class_weights = tf.placeholder(tf.float32, [None,2])
 
             # Recurrent layer
             with tf.name_scope("recurrent_layer"):
@@ -75,7 +78,7 @@ class BasicRecurrent(DetektorModel):
 
             # Gradient Descent
             self.learning_rate = tf.placeholder(shape=(), dtype=tf.float32, name="learning_rate")
-            self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.cost)
+            self.optimizer = optimizer(self.learning_rate).minimize(self.cost)
 
             # Run the initializer
             self._sess.run(tf.global_variables_initializer())
@@ -107,14 +110,25 @@ class BasicRecurrent(DetektorModel):
 
         # Make learning rates
         learning_rates = linear_geometric_curve(n=2000,
-                                                starting_value=1e-7,
+                                                starting_value=1e-3,
                                                 end_value=1e-18,
                                                 geometric_component=3. / 4,
                                                 geometric_end=5)
 
+        # Calc sample probability based on class-size
+        #TODO: Move this to own function and implement a "batch_strategy" input
+        non_claim_if = 1.0/sum(output_truth==0)
+        claim_if = 1.0/sum(output_truth==1)
+        sample_weights = np.empty((len(train_idx)))
+        sample_weights[output_truth == 0] = non_claim_if
+        sample_weights[output_truth == 1] = claim_if
+        sample_weights = sample_weights/sum(sample_weights) # normalize to yield probabilities
+
         # Run training batches
         for batch_nr in range(n_batches):
-            c_indices = np.random.choice(train_idx, batch_size, replace=False)
+            #c_indices = np.random.choice(train_idx, batch_size, replace=False)
+            c_indices = np.random.choice(train_idx, batch_size, replace=False,
+                                         p=sample_weights)
             c_inputs = input_tensor[c_indices, :, :]
             c_truth = output_truth[c_indices]
             c_input_lengths = input_lengths[c_indices]
---
+++ run_files/overfit_like_crazy.py
@@ -0,0 +1,116 @@
+## Overfit to the training data to see if we can learn anything
+#exec("../project_paths.py")
+import xarray as xr
+from evaluations import Accuracy, F1, TruePositives, TrueNegatives, FalsePositives, FalseNegatives, Samples
+import numpy as np
+from models.recurrent.basic_recurrent import BasicRecurrent
+from util.tensor_provider import TensorProvider
+import tensorflow as tf
+
+# Test-train parameters
+n_test_programs = 2
+
+# Initialize tensor-provider (data-source)
+the_tensor_provider = TensorProvider(verbose=True)
+
+# Initialize model
+REC_HIDDEN_UNITS = 200
+FC_HIDDEN_UNITS = 400
+ITERS=2000
+BATCH_SIZE=100
+recmodel = BasicRecurrent(the_tensor_provider, units=[REC_HIDDEN_UNITS, FC_HIDDEN_UNITS],
+                          optimizer=tf.train.AdamOptimizer, word_embedding=True,
+                          pos_tags=True, char_embedding=False)
+
+# Evaluation functions
+eval_functions = [Accuracy(), F1(), TruePositives(), TrueNegatives(), FalsePositives(), FalseNegatives(),
+                  Samples()]
+n_evaluations = len(eval_functions)
+
+classification_results_training = np.full((1, n_evaluations), np.nan)
+classification_results_training = xr.DataArray(classification_results_training,
+                                          name="Training Results",
+                                          dims=["Model", "Evaluation"],
+                                          coords=dict(Evaluation=[val.name() for val in eval_functions],
+                                                      Model=[recmodel.name()]))
+
+classification_results_test = np.full((1, n_evaluations), np.nan)
+classification_results_test = xr.DataArray(classification_results_test,
+                                          name="Test Results",
+                                          dims=["Model", "Evaluation"],
+                                          coords=dict(Evaluation=[val.name() for val in eval_functions],
+                                                      Model=[recmodel.name()]))
+
+# Elements keys
+keys = the_tensor_provider.keys
+
+# Get program ids and number of programs
+program_ids = np.array(list(zip(*keys))[0])
+unique_programs = np.array(sorted(set(program_ids)))
+
+# Select test-programs
+test_programs = np.random.choice(unique_programs, size=n_test_programs, replace=False)
+
+# Get split indices
+test_idx = 0
+for test_program in test_programs:
+    test_idx = test_idx + (program_ids == test_program)
+train_idx = np.where(test_idx < 0.5)[0]
+test_idx = np.where(test_idx > 0.5)[0]
+
+# Report
+print("Test programs {}, using {} training samples and {} test samples.".format(test_programs + 1,
+                                                                           len(train_idx),
+                                                                           len(test_idx)))
+
+# Make and set BoW-vocabulary
+bow_vocabulary = the_tensor_provider.extract_programs_vocabulary(train_idx)
+the_tensor_provider.set_bow_vocabulary(bow_vocabulary)
+
+# Get truth of test-set and train
+y_true_train = the_tensor_provider.load_labels(data_keys_or_idx=train_idx)
+y_true_test = the_tensor_provider.load_labels(data_keys_or_idx=test_idx)
+
+
+# Fit model
+recmodel.fit(tensor_provider=the_tensor_provider,
+          train_idx=train_idx,
+          verbose=2, batch_size=BATCH_SIZE, n_batches=ITERS)
+
+
+# Predict on train-data
+y_pred_train = recmodel.predict(tensor_provider=the_tensor_provider,
+                       predict_idx=train_idx)
+y_pred_train = np.squeeze(y_pred_train)
+
+
+# Predict on test-data for performance
+y_pred_test = recmodel.predict(tensor_provider=the_tensor_provider,
+                       predict_idx=test_idx)
+y_pred_test = np.squeeze(y_pred_test)
+
+
+# Evaluate with eval_functions
+for evaluation_nr, evalf in enumerate(eval_functions):
+    # Training data evaluation
+    assert y_pred_train.shape == y_true_train.shape, "Training: y_pred ({}) and y_true ({}) " \
+                                                   "do not have same shape".format(y_pred_train.shape, y_true_train.shape)
+
+    evaluation_results_train = evalf(y_true_train, y_pred_train)
+    classification_results_training[0, evaluation_nr] = evaluation_results_train
+
+    # Hold-out test data evaluation
+    assert y_pred_test.shape == y_true_test.shape, "Test: y_pred ({}) and y_true ({}) " \
+                                         "do not have same shape".format(y_pred_test.shape, y_true_test.shape)
+
+    evaluation_results_test = evalf(y_true_test, y_pred_test)
+    classification_results_test[0, evaluation_nr] = evaluation_results_test
+
+
+
+# Print mean results
+print("\nSingle training Results -- TRAINING --\n" + "-" * 75)
+print(classification_results_training._to_dataset_split("Model").to_dataframe())
+
+print("\nSingle training Results -- TEST --\n" + "-" * 75)
+print(classification_results_test._to_dataset_split("Model").to_dataframe())
